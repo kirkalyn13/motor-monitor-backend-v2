f6eaea79843b0dbfcd47913bd5ef87408a422d10
Kirk Santos <kirkalyn13@gmail.com>
2024-08-11 00:31:01 +0800

    initial commit

---
+++ src/main/java/com/engrkirky/motormonitorv2/Motormonitorv2Application.java
@@ -0,0 +1,13 @@
+package com.engrkirky.motormonitorv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Motormonitorv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Motormonitorv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/engrkirky/motormonitorv2/Motormonitorv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engrkirky.motormonitorv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Motormonitorv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
